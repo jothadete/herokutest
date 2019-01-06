0bc2c633bb6e278a73ecbcfb0f5f038b9c901b0b
Jens Tenter <jens.tenter@live.com>
2019-01-06 21:18:51 +0100

    Added Resource Basiszinssatz

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ class Basiszinssatz(Resource):
 	def get(self):
 		return basiszinssatz, 200
 
-api.add_resource(Basiszinssatz, "basiszinssatz")
+api.add_resource(Basiszinssatz, "/basiszinssatz")
 
 
 if __name__ == "__main__":
